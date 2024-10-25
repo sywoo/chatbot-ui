b06028a64b0305f919d0135dcf5be8d631f47832
sywoo <osy5940@naver.com>
2024-10-25 22:29:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if prompt := st.chat_input():
    
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = "답변"
+
+    msg = "답변" # 여기에 모델 답변 SET 하면 답변 출력됨
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
